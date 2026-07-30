e7db3b43e34c604d201744ad39977f2dfc2548ad
Thomas Hobbs <thomas.hobbs1205@gmail.com>
2026-07-30 10:42:09 +0930

    Add new task_priority.py script with welcome message.

---
+++ task_priority.py
@@ -0,0 +1,6 @@
+# Task Prioirity script - This program uses priority logic and loops to keep the program running
+# Thomas Hobbs
+
+# Display welcome message
+print("Welcome to Task Tracker Prioirity Checker!")
+print()
