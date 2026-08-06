3a2af4a670152474f9662d05936a7a7e9d8b0a25
Thomas Hobbs <thomas.hobbs1205@gmail.com>
2026-08-06 11:53:34 +0930

    Add UrgentTask subclass

--- task.py
+++ task.py
@@ -48,6 +48,7 @@ class Task:
         Convert the Task object into a dictionary for JSON.
         """
         return {
+            "type": "Task",
             "name": self.name,
             "priority": self.__priority,
             "estimated_time": self.estimated_time,
@@ -82,3 +83,38 @@ class Task:
             f"Status: {status} | "
             f"Est. Time: {self.estimated_time} mins"
         )
+    
+class UrgentTask(Task):
+    """
+    Represents an urgetn task with a deadline.
+    """
+    
+    def __init__(self, name, estimated_time, deadline):
+        """
+        Initialize an urgent task with a fixed high priority.
+        """
+        super().__init__(name, "high", estimated_time)
+        self.deadline = deadline
+    
+    def __str__(self):
+        """
+        Return a readable string representation of the urgent task.
+        """
+        status = "Done" if self.get_is_complete() else "Pending"
+        
+        return (
+            f"[URGENT] {self.name} | "
+            f"Status: {status} | "
+            f"Est. Time: {self.estimated_time} mins | "
+            f"Deadline: {self.deadline}"
+        )
+        
+    def to_dict(self):
+        """
+        Convert the urgent task into a dictionary for JSON.
+        """
+        task_dict = super().to_dict()
+        task_dict["type"] = "UrgentTask"
+        task_dict["deadline"] = self.deadline
+        
+        return task_dict
